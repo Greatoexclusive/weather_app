447755c50684380aa379c74441fb4b8ba3d8f9ed
greatoexclusive <greatoexclusive@gmail.com>
2022-05-23 21:13:10 +0100

    completed refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/core/constants/image_keys.dart';
+import 'package:weather_app/utils/allFunctions.dart';
 import 'package:weather_app/views/home/home_view.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  AllFunction _allFunction = AllFunction();
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Future.wait([
+      // preloading asset images
+      _allFunction.loadImg(context, ImageKeys.clear),
+      _allFunction.loadImg(context, ImageKeys.cloudy),
+      _allFunction.loadImg(context, ImageKeys.rainy),
+      _allFunction.loadImg(context, ImageKeys.stormy),
+
+      // loadSvg(context, "svg_2.svg"),
+    ]);
     return const MaterialApp(title: 'Weatherz Today', home: HomeView());
   }
 }
